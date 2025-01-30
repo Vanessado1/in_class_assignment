cad2458b752ff80df43e1c2d364b3c96cebfa9fd
Vanessa Do <xex.vanessa@gmail.com>
2025-01-30 16:17:26 -0500

    Completed Task 2

--- in_class_assignment_1.js
+++ in_class_assignment_1.js
@@ -1,2 +1,21 @@
 // Task 1
 
+let x = 10;
+if (x > 5) {
+    console.log("help");
+}
+
+let expenses = 10000;
+if (expenses > 7000) {
+    console.log("High Expenses");
+} else {
+    console.log( "Manageable Expenses");
+}
+
+// Task 2
+let score = 75;
+if (score >= 60) {
+    console.log("Passed");
+} else {
+    console.log("Failed");
+}
